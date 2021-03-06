5a883a2b0d1f3ab0e8f3e9c58a64513f5e096ff3
Michael Bradvica <mjbradvica@protonmail.com>
2021-03-05 18:50:03 -0800

    storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/preset-create-react-app',
+		'@storybook/addon-actions',
+		'@storybook/addon-knobs',
+		'@storybook/addon-a11y',
+		'@storybook/addon-jest',
+	],
+};
