dfcaf3f5a79bc2675897eebc767b33022189eb92
Michael Bradvica <mjbradvica@protonmail.com>
2021-03-05 16:34:07 -0800

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  arrowParens: "avoid",
+  printWidth: 150,
+  semi: true,
+  jsxBracketSameLine: true,
+  useTabs: true,
+  tabWidth: 4,
+  singleQuote: true,
+};
