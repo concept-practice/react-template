e2a63b78149f100cf7c2b798963bba0849a7e2f6
Michael Bradvica <mjbradvica@protonmail.com>
2021-11-05 11:58:44 -0400

    eslint and prettier updates

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  trailingComma: "es5",
-  arrowParens: "always",
-  printWidth: 150,
-  semi: true,
-  jsxBracketSameLine: true,
-  useTabs: true,
-  tabWidth: 4,
-  singleQuote: true,
+	trailingComma: 'es5',
+	arrowParens: 'always',
+	printWidth: 150,
+	semi: true,
+	bracketSameLine: true,
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
 };
